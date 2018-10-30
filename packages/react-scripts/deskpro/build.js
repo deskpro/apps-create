ccd1d32ffd735dde523fd2b3b59d7d9044fff94c
radu <radu.helstern@deskpro.com>
2018-10-30 17:47:10 +0000

    add pre-render capabilities to the build stage

--- packages/react-scripts/deskpro/build.js
+++ packages/react-scripts/deskpro/build.js
@@ -1,57 +1,8 @@
-const chalk = require('chalk');
-const printBuildError = require('react-dev-utils/printBuildError');
-const path = require('path');
-const paths = require('../config/paths');
-
-const installer = require('./installer');
-const docs = require('./docs');
-const packager = require('./packager');
-const manifest = require('./manifest');
+const bundle = require('./bundle');
+const render = require('./render');
 
 module.exports = function(data) {
-  return Promise.resolve(data)
-    .then(
-      () =>
-        manifest.extractAndwrite(
-          paths.appPackageJson,
-          path.resolve(paths.appBuild, 'manifest.json')
-        ),
-      err => {
-        console.log(chalk.red('Failed to write the deskpro app manifest.\n'));
-        printBuildError(err);
-        process.exit(1);
-      }
-    )
-    .then(
-      // bundle the installer
-      () =>
-        installer.useBundled
-          ? installer.bundle(paths.deskproInstallerPackage, paths.appBuild)
-          : null,
-      err => {
-        console.log(chalk.red('Failed to bundle the installer manifest.\n'));
-        printBuildError(err);
-        process.exit(1);
-      }
-    )
-    .then(
-      // bundle the docs
-      () =>
-        docs.bundle(path.resolve(paths.appPackageJson, '..'), paths.appBuild),
-      err => {
-        console.log(chalk.red('Failed to bundle application docs.\n'));
-        console.log(err);
-        printBuildError(err);
-        process.exit(1);
-      }
-    )
-    .then(
-      // package the app
-      () => packager(path.resolve(paths.appPackageJson, '..'), 'app.zip'),
-      err => {
-        console.log(chalk.red('Failed to package the app.\n'));
-        printBuildError(err);
-        process.exit(1);
-      }
-    );
+  return process.env.DP_STATIC_RENDER === 'on'
+    ? render(data).then(bundle)
+    : bundle(data);
 };
