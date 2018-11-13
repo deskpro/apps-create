ff0dfdcfccbe84aa3693803a8e06b6115ddadd1b
radu <radu.helstern@deskpro.com>
2018-11-13 16:42:23 +0000

    modify the jest configuration to allow transpiling of date-fns make preloading on by default

--- packages/react-scripts/deskpro/build.js
+++ packages/react-scripts/deskpro/build.js
@@ -2,7 +2,7 @@ const bundle = require('./bundle');
 const render = require('./render');
 
 module.exports = function(data) {
-  return process.env.DP_STATIC_RENDER === 'on'
-    ? render(data).then(bundle)
-    : bundle(data);
+  return process.env.DP_STATIC_RENDER === 'off'
+    ? bundle(data)
+    : render(data).then(bundle);
 };
--- packages/react-scripts/deskpro/createJestConfig.js
+++ packages/react-scripts/deskpro/createJestConfig.js
@@ -7,5 +7,12 @@ module.exports = (resolve, rootDir, isEjecting) => {
     '<rootDir>/test/**/?(*.)(spec|test).{js,jsx,mjs}'
   );
 
+  // see https://jestjs.io/docs/en/tutorial-react-native#transformignorepatterns-customization
+  // we're overriding the default transformIgnorePatterns regex with one that allows some packages
+  // to extend this regex for other packages that need transpiling replace `date-fns` with:
+  // date-fns|<name-of-package>
+  config.transformIgnorePatterns = [
+    '[/\\\\]node_modules[/\\\\](?!(date-fns)[/\\\\]).+\\.(js|jsx|mjs)$',
+  ];
   return config;
 };
